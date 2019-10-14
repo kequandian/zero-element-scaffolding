6c47c73c563eb3ded14b177df2d7309d629dee38
LLH <youyeke@163.com>
2019-10-14 13:24:55 +0800

    修改 public config 默认值

--- public/config.js
+++ public/config.js
@@ -2,7 +2,7 @@ if (window.ZEle === undefined) {
   window.ZEle = {};
 }
 window.ZEle.endpoint = "http://192.168.0.1:8080";
-window.ZEle.nav = "both";
+window.ZEle.nav = "left";
 window.ZEle.theme = "#1890ff";
 
 window.ZEle.remoteConfig = {};
