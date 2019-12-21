9fddb1b8bb23a368a6bd2d4d1a27e4c91d799827
LLH <youyeke@163.com>
2019-12-21 15:08:17 +0800

    可修改个人信息、重置邮箱

--- src/config/profile.config.js
+++ src/config/profile.config.js
@@ -1,7 +1,12 @@
 module.exports = [
   {
-    name: '修改密码',
-    icon: 'key',
+    name: '个人信息',
+    icon: 'idcard',
+    path: '/profile/baseInfo'
+  },
+  {
+    name: '账号安全',
+    icon: 'safety-certificate',
     path: '/profile/security'
   },
 ]
--- src/pages/profile/security.js
+++ src/pages/profile/security.js
@@ -3,11 +3,10 @@ import useBreadcrumb from '@/framework/useBreadcrumb';
 import ZEle from 'zero-element';
 
 const config = {
-  layout: 'Content',
-  title: '修改密码',
+  layout: 'Empty',
   items: [
     {
-      layout: 'Empty',
+      layout: 'Content',
       component: 'BaseForm',
       config: {
         goBack: false,
@@ -15,12 +14,53 @@ const config = {
           createAPI: '/api/u/crud/accounts/reset_password',
         },
         fields: [
-          { field: 'oldPassword', label: '原密码', type: 'password' },
-          { field: 'newPassword', label: '新密码', type: 'password' },
-          { field: 'repeatpPassword', label: '重复新密码', type: 'password' },
+          { field: 'group', label: '', type: 'group', value: '修改密码' },
+          {
+            field: 'oldPassword', label: '原密码', type: 'password',
+            rules: ['required'],
+          },
+          {
+            field: 'newPassword', label: '新密码', type: 'password',
+            rules: ['required'],
+          },
+          {
+            field: 'repeatpPassword', label: '重复新密码', type: 'password',
+            rules: ['required'],
+          },
         ]
       }
-    }
+    },
+    {
+      layout: 'Content',
+      component: 'BaseForm',
+      config: {
+        goBack: false,
+        API: {
+          createAPI: '/api/u/crud/accounts/resetEmail',
+        },
+        layout: 'Grid',
+        fields: [
+          { field: 'group', label: '', type: 'group', value: '修改邮箱' },
+          {
+            field: 'email', label: '新邮箱', type: 'input',
+            rules: ['required', 'mail']
+          },
+          {
+            field: 'validateCode', label: '验证码', type: 'captcha', span: 8,
+            options: {
+              field: 'email',
+            },
+            props: {
+              style: {
+                width: 240,
+              }
+            },
+            rules: ['required'],
+          },
+        ]
+      }
+    },
+
   ]
 };
 
@@ -28,7 +68,7 @@ export default function (props) {
   useBreadcrumb(props, [
     { title: '首页', path: '/' },
     { title: '个人中心' },
-    { title: '修改密码' },
+    { title: '账号安全' },
   ]);
 
   return <ZEle namespace='security_password' config={config} />
