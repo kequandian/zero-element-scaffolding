2268d2c75ac151f97478a92dd736de1f5ba9f527
LLH <youyeke@163.com>
2020-08-07 11:48:30 +0800

    修改默认 endpoint

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 if (window.ZEle === undefined) {
   window.ZEle = {};
 }
-window.ZEle.endpoint = "http://192.168.0.1:8080";
+window.ZEle.endpoint = "http://127.0.0.1:8080";
 window.ZEle.nav = "left";
 window.ZEle.theme = "#1890ff";
 window.ZEle.indexPage = "";
