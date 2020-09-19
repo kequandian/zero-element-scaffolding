095e5472b047c413642610224dc9a2fa2e5ee762
LLH <youyeke@163.com>
2020-09-19 16:41:40 +0800

    修改注册字段

--- src/pages/login/components/MailReg.js
+++ src/pages/login/components/MailReg.js
@@ -1,43 +1,41 @@
 import React, { Component } from 'react';
 import { Form, Input, Button } from 'antd';
-import { MailOutlined, ClockCircleOutlined } from '@ant-design/icons';
+import { PhoneOutlined, KeyOutlined } from '@ant-design/icons';
 
 import styles from '../index.less';
 import Captcha from './Captcha';
 
-export default class MailRegForm extends Component {
-  render() {
-    const { onReg, loading } = this.props;
+export default function MailRegForm(props) {
+  const { onReg, loading } = props;
+  const [form] = Form.useForm();
 
-    return (
-      <Form onFinish={onReg} className={styles.Form}>
-        <Form.Item name="email" rules={[{ required: true, message: '请输入邮箱' }]}>
-          <Input
-            prefix={<MailOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
-            placeholder="邮箱"
-          />
-        </Form.Item>
-        <Form.Item name="password" rules={[{ required: true, message: '请输入密码' }]}>
-          <Input
-            prefix={<ClockCircleOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
-            type="password"
-            placeholder="密码"
-          />
-        </Form.Item>
-        <Form.Item name="validateCode" rules={[{ required: true, message: '请输入验证码' }]}>
-          <Captcha
-            type="email"
-          // receiver={getFieldValue('email')}
-          />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit" className={styles.Button}
-            loading={loading}
-          >
-            注册
-          </Button>
-        </Form.Item>
-      </Form>
-    );
-  }
+  return (
+    <Form form={form} onFinish={onReg} className={styles.Form}>
+      <Form.Item name="phone" rules={[{ required: true, message: '请输入手机号' }]}>
+        <Input
+          prefix={<PhoneOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
+          placeholder="手机号"
+        />
+      </Form.Item>
+      <Form.Item name="validateCode" rules={[{ required: true, message: '请输入验证码' }]}>
+        <Captcha
+          type="phone"
+          receiver={form.getFieldValue('email')}
+        />
+      </Form.Item>
+      <Form.Item name="password" rules={[{ required: true, message: '请输入组织Code' }]}>
+        <Input
+          prefix={<KeyOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
+          placeholder="组织Code"
+        />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className={styles.Button}
+          loading={loading}
+        >
+          注册
+        </Button>
+      </Form.Item>
+    </Form>
+  )
 }
