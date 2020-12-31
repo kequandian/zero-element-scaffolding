342b552a7f22443e1433a07f2241e45f03caa462
LLH <youyeke@163.com>
2020-12-31 18:17:29 +0800

    授权逻辑修改

--- src/pages/login/utils/getUserInfo.js
+++ src/pages/login/utils/getUserInfo.js
@@ -2,11 +2,13 @@ import { query } from 'zero-element-antd/lib/utils/request';
 import { saveToken } from 'zero-element/lib/utils/request/token';
 
 export default function getUserInfo() {
-  return query('/api/u/crud/accounts/userInfo')
+  return query('/api/adm/users/userInfo')
     .then(data => {
       saveToken({
         userName: data.name,
         avatar: data.avatar,
+        avatar: data.avatar,
+        extra: data.name,
         remember: true,
       });
     })
