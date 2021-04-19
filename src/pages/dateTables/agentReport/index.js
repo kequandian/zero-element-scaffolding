3d6e031ae4615be332aeb678c41755a588927245
18219300257 <2453909581@qq.com>
2021-04-19 15:46:20 +0800

    完善了报表可视化的模块

--- src/pages/dateTables/agentReport/index.js
+++ src/pages/dateTables/agentReport/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ZEle from 'zero-element';
-import config from './config';
+import config from './config/advertiserReport-setting';
 
 export default function AgentReport() {
   return <ZEle namespace='agentReport' config={config} />;
