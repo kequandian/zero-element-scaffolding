fade39610d94f710325795999aa4d3e75d65ef62
HDJ <263463764@qq.com>
2021-04-27 14:21:30 +0800

    权限API代码 新增 开发，生产模式 访问次数判断

--- src/models/global.js
+++ src/models/global.js
@@ -28,8 +28,10 @@ createModel({
     },
     queryPerm: async function () {
       if (getToken()) {
-        //限制只访问三次
-        if(this.getRequestCount() >= 3){
+        
+        if(process.env.NODE_ENV === 'development' && this.getRequestCount() >= 3){ //开发模式 并 限制只访问三次
+          return;
+        }else if(process.env.NODE_ENV === 'production' && this.getRequestCount() >= 3){ //生成模式 并 限制只访问三次
           return;
         }
         if (!this.permissions || Array.isArray(this.permissions)) {
