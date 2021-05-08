f4f63fc166d61aafe7c99d95d19791d08a1c1dea
AEdgejay <471021444@qq.com>
2021-05-08 19:08:52 +0800

    增加了tag样式

--- src/components/SchemaGeneratorPage/components/form/serialNumber.js
+++ src/components/SchemaGeneratorPage/components/form/serialNumber.js
@@ -5,7 +5,7 @@ export default function(){
     const M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1);
     const D = (date.getDate() < 10 ? '0'+(date.getDate()) : date.getDate()); 
 
-    return <Tag color="blue">ZF{Y+
+    return <Tag className="_tag_color_status_processing">ZF{Y+
     M+D
     }01</Tag>
 }
