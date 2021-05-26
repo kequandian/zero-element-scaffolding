9e4ae256ce73db9f18b454014ab898f609c250c0
HDJ <263463764@qq.com>
2021-05-26 16:08:10 +0800

    更换 待办事项->获取详情api

--- src/framework/PrimaryLayout/Login/index.js
+++ src/framework/PrimaryLayout/Login/index.js
@@ -103,7 +103,7 @@ export default (props) => {
   const userMenu = (
     <Menu>
       <Menu.Item style={{ padding: 0 }} key="userMenu">
-        <TestUserSelection onItemClickHandle={handleOnItemClickHandle} />
+        <TestUserSelection onItemClickHandle={() => handleOnItemClickHandle()} />
       </Menu.Item>
     </Menu>
   )
