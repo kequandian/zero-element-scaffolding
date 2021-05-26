17603691640668e1d874fb72468da70411007a00
AEdgejay <471021444@qq.com>
2021-05-26 19:51:42 +0800

    迁移tabels，测试，增加后台配置页

---
+++ src/framework/PrimaryLayout/Login/svg/Svg.js
@@ -0,0 +1,11 @@
+
+export const ConfigSvg = (props) => {
+    const {
+        width='16',
+        height="16",
+        color ="#fff",
+        Opacity="1"
+    }=props
+
+    return <svg t="1622017129139" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="7030" width={width} height={height}><path d="M903.5 958.9H121.1c-30.9 0-55.9-25-55.9-55.9V120.7c0-30.9 25-55.9 55.9-55.9h782.3c30.9 0 55.9 25 55.9 55.9V903c0 30.9-25 55.9-55.8 55.9zM773.1 474.6H549.6V251.1H475v223.5H251.5v74.5H475v223.5h74.5V549.1H773v-74.5z" fill={color} p-id="7031"></path></svg>
+}
