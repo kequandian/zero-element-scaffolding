127481655a7da1cf2e9b865870d00853ff7bbc8d
HDJ <263463764@qq.com>
2021-05-27 16:09:54 +0800

    更换 提交审核数据 api

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,9 +7,6 @@ import { BackTop } from 'antd';
 import router from '@/config/router.config';
 import profileMenuData from '@/config/profile.config';
 import toDoListMenuData from '@/config/toDoList.config';
-import { useDocumentVisibility } from 'ahooks';
-import { useModel, getModel } from 'zero-element/lib/Model';
-// import { LS } from 'zero-element/lib/utils/storage';
 
 let menuData = [...router];
 
@@ -39,19 +36,6 @@ function BasicLayout(props) {
   const { location } = props;
   const { pathname } = location;
 
-  const documentVisibility = useDocumentVisibility();
-  // const menuConfigModel = useModel('menuConfig');
-  // const [ menuFirstRequest, setMenuFirstRequest ] = useState(0);
-  // const [ menuFirstPush, setMenuFirstPush ] = useState(0);
-  
-  // useEffect(_ => {
-    
-  //   if (documentVisibility === 'visible') {
-  //     menuConfigModel.queryMenu();
-  //     setMenuFirstRequest(1);
-  //   }
-  // }, [menuTree, documentVisibility]);
-
   const [state, dispatch] = useReducer(reducer, {
     breadcrumb: [],
     style: {
@@ -75,16 +59,6 @@ function BasicLayout(props) {
       type: 'clearBc',
     });
   }
-
-  // console.log('menuTree = ', menuTree)
-  //更新菜单信息
-  // const menuTree = LS.get('menuList');
-  // if(menuFirstRequest == 1 && menuFirstPush == 0 && menuTree.length > 0){
-  //   if(Array.isArray(menuTree)){
-  //     menuData = menuTree;
-  //     setMenuFirstPush(1)
-  //   }
-  // }
 
   return (
     <GlobalContext.Provider value={state}>
