14a37034d57aab5386e44131b6cc3a2725a6c05f
HDJ <263463764@qq.com>
2021-05-28 15:06:03 +0800

    修改 部分文字描述

--- src/pages/userAudit/applyManage/config/applyManage-apply/approveFormConfig.js
+++ src/pages/userAudit/applyManage/config/applyManage-apply/approveFormConfig.js
@@ -36,7 +36,7 @@ module.exports = {
             rules: ['required']
         },
         {
-            label: '办理意见', field: 'currentStepName', type: 'text-area',
+            label: '备注', field: 'currentStepName', type: 'text-area',
             span: 12,
             props: {
                 autoSize: {
