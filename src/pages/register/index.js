94a1f42988da587ed663942b896da1777a6cea27
djhuang <35095695+h89916300@users.noreply.github.com>
2021-07-21 18:02:33 +0800

    Update index.js

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -3,21 +3,25 @@ import { history } from 'umi';
 import { message } from 'antd';
 import qs from 'qs';
 import { getToken, saveToken } from 'zero-element/lib/utils/request/token';
-import { useModel } from 'zero-element/lib/Model';
+// import { useModel } from 'zero-element/lib/Model';
 
 export default function (props) {
 
     const { location } = props;
     const { token } = qs.parse(location.search.replace('?', ''));
-    const model = useModel('global');
+    // const model = useModel('global');
 
     if(token){
         saveToken({
             token,
         })
-        model.setRequestCount(3)
-        //model.queryPerm(false);
-        history.push('/admin');
+        // model.queryPerm(false);
+        history.push({
+            pathname: '/admin',
+            search:{
+                permStatus: false
+            }
+        });
     }else{
         message.error('token不能为空')
     }
