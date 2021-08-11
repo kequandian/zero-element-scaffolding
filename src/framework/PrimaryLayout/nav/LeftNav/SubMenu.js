909c2b2750ead1075f5073214f69d3678029a9b8
AEdgejay <471021444@qq.com>
2021-08-11 12:44:31 +0800

    添加左侧栏展示事件

--- src/framework/PrimaryLayout/nav/LeftNav/SubMenu.js
+++ src/framework/PrimaryLayout/nav/LeftNav/SubMenu.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 export default function SubMenu({ selected, title, children }) {
   const classes = [
     'nav-item-container',
     selected ? 'selected' : '',
   ].join(' ');
+  const [show,setShow] = useState(false)
+  const handleShow=()=>{
+    setShow(!show)
+    return show
+  }
 
   return <div className={classes}>
-    <div>{title}</div>
-    <div>{children}</div>
+    <div className="nav-titleBox" onClick={handleShow}>{title}</div>
+    <div style={{display:show?"block":"none"}}>{children}</div>
   </div>
 }
