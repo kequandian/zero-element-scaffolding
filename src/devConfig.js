6ec2d8f7a6cf53e380cfb9e67f581ac9f4531725
HDJ <263463764@qq.com>
2021-09-13 09:58:17 +0800

    default

--- src/devConfig.js
+++ src/devConfig.js
@@ -6,6 +6,7 @@
 */
 export const Config ={
     // endpoint:'http://192.168.3.239:8090'
-    // endpoint:"http://sandbox.f.gateway.host.smallsaas.cn:81/"
-    endpoint:"http://demo.smallsaas.cn"
+    endpoint:'http://192.168.3.239:8000'
+    // endpoint:"http://sandbox.f.gateway.host.smallsaas.cn:81"
+    // endpoint:"http://demo.smallsaas.cn"
 }
