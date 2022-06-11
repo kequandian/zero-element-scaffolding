4b68bbcd278060ed93b6c9942fafa7ec85679bee
HDJ <263463764@qq.com>
2022-06-11 11:58:36 +0800

    form-type-starter 新增 gen.js

--- form-type-starter/src/pages/index.js
+++ form-type-starter/src/pages/index.js
@@ -1,23 +1,11 @@
 import React from 'react';
-import { history } from 'umi';
-import win from 'zero-element/lib/utils/window';
 
-import { getToken } from 'zero-element/lib/utils/request/token';
+import FormItemTypeManage from './formItemTypeManage'
 
 export default function () {
-  // if (win.ZEle.indexPage) {
-  //   history.push(win.ZEle.indexPage);
-  // }
-
-  // if (getToken()) {
-    // history.push('/home');
-  // } else {
-    history.push('/formItemTypeManage');
-  // }
+    // history.push('/formItemTypeManage');
 
   return (
-    <div>
-      首页
-    </div>
+    <FormItemTypeManage/>
   );
 }
