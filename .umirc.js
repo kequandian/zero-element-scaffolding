380835c00e8ad708eefb1f6c9da5a828e50f2794
m.2@sandbox <m.2@sandbox>
2022-11-03 15:45:21 +0800

    add 如何打包为目录结构.md

--- .umirc.js
+++ .umirc.js
@@ -1,7 +1,7 @@
 
 // ref: https://umijs.org/config/
 export default {
-  title: 'SmallSaaS低代码开发平台',
+  title: 'Web-Tool',
   hash: true,
   // history: {
   //   type: 'hash',
@@ -19,4 +19,15 @@ export default {
   antd: {},
   dva: false,
   ignoreMomentLocale: true, // 忽略 moment 的 locale 文件
+
+  history: {
+    type: 'hash'
+  },
+  chainWebpack(memo, { env, webpack, createCSSRule }) {
+     memo.output.set('path', require('path').resolve(__dirname, 'dist/swagger-manage'))
+     memo.output.set('filename', 'bundle.js')
+  },
+   
+  // outputPath: '/dist/swagger-manage',
+  publicPath: process.env.NODE_ENV === 'production' ? './swagger-manage/' : '/',  //设置 dist/index.html 访问 js和css路径
 }
