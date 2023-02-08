aac29fdf651b4ced670e327d05557f3ab2679016
hdj <263463764@qq.com>
2023-02-08 17:17:50 +0800

    新增图片管理

--- src/devConfig.js
+++ src/devConfig.js
@@ -8,8 +8,8 @@ export const Config ={
     // endpoint:"http://192.168.3.194:8081",//api接口endpoint 
     // endpoint:"http://49.119.119.7:8001",//api接口endpoint
     // endpoint:"http://app1.console.smallsaas.cn:8003",//api接口endpoint
-    // endpoint:"https://house.cloud.smallsaas.cn",//api接口endpoint
-    endpoint:"http://demo.smallsaas.cn:8001",//api接口endpoint
+    endpoint:"https://house.cloud.smallsaas.cn",//api接口endpoint
+    // endpoint:"http://demo.smallsaas.cn",//api接口endpoint
     breadcrumbType:"header", //header,top
     theme:"LeftCover",//TopCover,LeftCover
     aloneWindow:[   // 使用方法:在这里增加页面路径即可，如 /login
---
+++ src/pages/imageManage/imageManage/index.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import ZEle from 'zero-element';
+import useBreadcrumb from '@/framework/useBreadcrumb';
+
+const formFields = require('./formFields');
+
+const config = {
+  layout: 'Content',
+  title: '图片管理',
+  items: [
+    {
+      layout: 'Empty',
+      component: 'Search',
+      config: {
+        fields: [
+          { field: 'name', label: '图片名称', type: 'input', placeholder: '请输入' },
+        ]
+      }
+    },
+    {
+      component: 'Table', //subCategoryList
+      config: {
+        API: {
+          listAPI: '/api/gw/portal/images',
+          deleteAPI: '/api/gw/portal/images/(id)'
+        },
+        actions: [
+        /*  {
+            title: '添加', type: 'modal',
+            options: {
+              modalTitle: '添加图片',
+              modalWidth: 500,
+              items: [
+                {
+                  layout: 'Empty',
+                  component: 'Form',
+                  config: {
+                    layout: 'Grid',
+                    API: {
+                      createAPI: '/api/gw/portal/uploadImages'
+                    },
+                    fields: formFields,
+                  }
+                }
+              ]
+            }
+          },*/
+          // {
+          //   title: '生成 HTML',
+          //   type: 'request',
+          //   options: {
+          //     API: '/api/gw/portal/images/replace',
+          //     method: 'put',
+          //   },
+          // }
+        ],
+        fields: [
+          { "label": "编号(索引)", "value": "index" },
+          {
+            "field": "cover",
+            "label": "图片",
+            "valueType": "image"
+          },
+          {
+            label: "名称",
+            field: "name"
+          }
+        ],
+        operation: [
+          {
+            title: '编辑', action: 'modal',
+            options: {
+              outside: true,
+              modalTitle: '编辑图片',
+              modalWidth: 500,
+              layout: 'Empty',
+              items: [
+                {
+                  layout: 'Empty',
+                  component: 'Form',
+                  config: {
+                    layout: 'Grid',
+                    API: {
+                      getAPI: '/api/gw/portal/images/(id)',
+                      updateAPI: '/api/gw/portal/images/(id)',
+                    },
+                    fields: formFields,
+                  }
+                }
+              ]
+            }
+          }
+        ],
+      },
+    },
+  ]
+}
+
+export default ()=>{
+  return <ZEle namespace="img_manage" config={config} />
+};
