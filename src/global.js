782d5b267e42384190c8b0a019d78117f050f3fa
hdj <263463764@qq.com>
2023-11-03 10:41:23 +0800

    新增頁面配置管理頁面

--- src/global.js
+++ src/global.js
@@ -150,10 +150,10 @@ if (process.env.NODE_ENV === 'development') {
   // });
   window._env_ = {
     // REACT_APP_API_HOST: "192.168.3.175:6881",//ZLMediaKit服务的地址，默认端口为80
-    REACT_APP_API_HOST: Config.akstreamEndpoint,//ZLMediaKit服务的地址，默认端口为80
+    REACT_APP_API_HOST: "202.63.172.43:6881",//ZLMediaKit服务的地址，默认端口为80
     ZlMediaKit_Secret:"035c73f7-bb6b-4889-a715-d9eb2d1925cc",//ZLMediaKit配置文件的Secret
     // AKSTREAM_WEB_API:"192.168.3.175:5800",//AKStreamWeb服务器的地址，默认端口为5800
-    AKSTREAM_WEB_API:Config.akstreamEndpoint,//AKStreamWeb服务器的地址，默认端口为5800
+    AKSTREAM_WEB_API:"202.63.172.43:5800",//AKStreamWeb服务器的地址，默认端口为5800
     AKStream_AccessKey:"047I4WS1-U51UBO6W-1J4BT21P-MF17IT99-92J8WIHU-944Q4KIW"//AKStreamWeb配置文件的AccessKey
   }
 }else {
