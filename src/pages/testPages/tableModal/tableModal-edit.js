5da5637b686db29b73fd9f39938e8e9ccb50564a
hdj <263463764@qq.com>
2023-12-01 18:02:45 +0800

    测试 apicode

---
+++ src/pages/testPages/tableModal/tableModal-edit.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ZEle from 'zero-element';
+import config from './config/tableModal-edit';
+
+import useBreadcrumb from '@/framework/useBreadcrumb';
+import { useWillUnmount } from 'zero-element/lib/utils/hooks/lifeCycle';
+import switchEndpoint from '@/components/switchEndpoint';
+
+export default function WorkFlowListApply() {
+
+    useBreadcrumb([
+        { title: '首页', path: '/' },
+        { title: '实体管理', path: '/tableModal' },
+        { title: '编辑' },
+    ]);
+    useWillUnmount(switchEndpoint)
+
+    return <ZEle namespace="tableModal_edit" config={config} />
+};
