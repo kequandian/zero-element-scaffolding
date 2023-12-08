3e005b194a75ed4bc624c494e7cde8b4d13713c7
hdj <263463764@qq.com>
2023-12-08 17:21:38 +0800

    新增 字段模型管理 模块

--- src/pages/nocode/fieldModal/fieldModal-edit.js
+++ src/pages/nocode/fieldModal/fieldModal-edit.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ZEle from 'zero-element';
-import config from './config/tableModal-edit';
+import config from './config/fieldModal-edit';
 
 import useBreadcrumb from '@/framework/useBreadcrumb';
 import { useWillUnmount } from 'zero-element/lib/utils/hooks/lifeCycle';
@@ -10,10 +10,10 @@ export default function WorkFlowListApply() {
 
     useBreadcrumb([
         { title: '首页', path: '/' },
-        { title: '表模型管理', path: '/tableModal' },
+        { title: '字段模型管理', path: '/fieldManage' },
         { title: '编辑' },
     ]);
     useWillUnmount(switchEndpoint)
 
-    return <ZEle namespace="tableModal_edit" config={config} />
+    return <ZEle namespace="fieldManage_edit" config={config} />
 };
