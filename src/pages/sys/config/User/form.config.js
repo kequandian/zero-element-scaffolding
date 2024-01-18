4aa1ab8e2470bcda617271315f8a967b609d012f
hhhhhtao <1799231213@qq.com>
2024-01-18 18:10:37 +0800

    修改 添加用户功能

--- src/pages/sys/config/User/form.config.js
+++ src/pages/sys/config/User/form.config.js
@@ -1,9 +1,11 @@
 module.exports = [
   {
-    field: 'account', label: '登录账号', type: 'input',rules: ['required']
+    field: 'account', label: '登录账号', type: 'input', rules: ['required']
   },
- {
-    "label": "选择组织",
+
+  // added in 2024-1-17 将“选择组织”功能替换为“选择部门”
+  {
+    "label": "选择部门",
     "field": "orgId",
     "type": "modal-radio",
     "props": {},
@@ -11,22 +13,29 @@ module.exports = [
       "required"
     ],
     "options": {
-      "title": "选择组织",
+      "title": "选择部门",
       "value": "id",       //最终传入的数据
-      "API": "/api/sys/org",
+      "API": "/api/sys/org/department",
       "label": "name",       //编辑的时候返显
-      "editLabel": "org",   //打开的时候显示
       pagination: true,
       "fields": [
         {
-          "label": "组织名",
+          "label": "部门",
           "field": "name"
         }
       ]
     }
   },
+  // added in 2024-1-17 增加是否是部门经理按钮
   {
-    field: 'roleIds', label: '角色', type: 'modal-checkbox', 
+    field: 'isDepartmentManger', label: '是否为部门经理', type: 'radio', rules: ['required'], options: [
+      { label: '是', value: 1 },
+      { label: '否', value: 0 },
+    ]
+  },
+
+  {
+    field: 'roleIds', label: '角色', type: 'modal-checkbox',
     options: {
       API: '/api/adm/roles',
       title: '选择角色',
@@ -38,22 +47,19 @@ module.exports = [
     rules: ['required'],
   },
   {
-    field: 'name', label: '用户名', type: 'input',rules: ['required']
+    field: 'name', label: '用户名', type: 'input', rules: ['required']
   },
-  {
-    field: 'phone', label: '电话', type: 'input', 
-    rules: ['required'],
-  },
-
   {
     field: 'password', label: '密码', type: 'input', span: 12,
     rules: ['required'],
   },
- 
+  {
+    field: 'phone', label: '电话', type: 'input'
+  },
   {
     field: 'email', label: '邮箱', type: 'input',
     // rules: ['required'],
-    
+
   },
   {
     field: 'avatar', label: '头像', type: 'avatars',
@@ -63,17 +69,17 @@ module.exports = [
     },
   },
 
-/*  {
-    field: 'phone', label: '电话', type: 'input',
-    span: 12
-  },*/
+  /*  {
+      field: 'phone', label: '电话', type: 'input',
+      span: 12
+    },*/
   {
     field: 'sex', label: '性别', type: 'radio',
     options: [
       { label: '男', value: '0' },
       { label: '女', value: '1' }
     ],
-    
+
   },
   {
     field: 'birthday', label: '出生年月', type: 'date',
